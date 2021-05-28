655c5303a3db0b00cca7d0e6b63a7cd5c3791d4e
abdul ghani <mohammedghani842@gmail.com>
2021-05-27 21:20:18 -0700

    completed raw to clean

--- ds.js
+++ ds.js
@@ -4,25 +4,28 @@
 // const foobar = foo.reduce((a,b) => ({...a, [b]:undefined}), {});
 // const foobaz = [...Object.keys(foobar),].join('');
 // console.log(foobaz)
-function countWords(str, words,){
-    let count = 0;
-    // let counts = 0;
-    for(i=0; i<str.length; i++){
-        if(str.charAt(i) == words){
-            count +=1;
-        }
+// function countWords(str, words,){
+//     let count = 0;
+//     // let counts = 0;
+//     for(i=0; i<str.length; i++){
+//         if(str.charAt(i) == words){
+//             count +=1;
+//         }
        
 
-    }
-    return count;
-}
-    let string="ttttwww wo the"
-    let word="t"
-    // let leter = "w"
-    console.log(countWords(string, word,))
+//     }
+//     return count;
+// }
+//     let string="ttttwww wo the"
+//     let word="t"
+//     // let leter = "w"
+//     console.log(countWords(string, word,))
     
-   
-
+// Write a function called cleanText. The function takes raw text as a parameter and returns the clean text.
+// let strin1 = "what is \n your name"
+// console.log(strin1)
+// let strin = String.raw`what is your \n name my name is `
+// console.log(strin)
 
      
 
